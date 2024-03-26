fbae284216729810881ac697757b0045f11b747d
Soumyadeep Sikder <141468565+soumyadeepsikder10@users.noreply.github.com>
2024-03-26 16:22:59 +0000

    datatypes

---
+++ 01_JSbasics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14455
+let accountEmail = "ben10@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+//dont use var bcz of scope issue
+
+accountEmail = "rik20@gmail.com"
+accountPassword = "55555"
+accountCity = "Barasat"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); 
+
+//node 01_JSbasics/01_variables.js
